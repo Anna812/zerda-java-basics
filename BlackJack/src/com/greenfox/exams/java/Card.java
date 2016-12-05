a528cde3b2ff49e933d733ff15a732e0699261ca
Anna812 <88menyus@gmail.com>
2016-12-05 09:58:32 +0100

    update Card Class' fields
    create constructor and field for Deck Class

--- BlackJack/src/com/greenfox/exams/java/Card.java
+++ BlackJack/src/com/greenfox/exams/java/Card.java
@@ -4,10 +4,9 @@ package com.greenfox.exams.java;
  * Created by Anna812 on 12/5/2016.
  */
 public class Card {
-    private String colour;
-    private int value;
+    private String colour, value;
 
-    public Card(String colour, int value) {
+    public Card(String colour, String value) {
         this.colour = colour;
         this.value = value;
     }
--- BlackJack/src/com/greenfox/exams/java/Deck.java
+++ BlackJack/src/com/greenfox/exams/java/Deck.java
@@ -1,7 +1,29 @@
 package com.greenfox.exams.java;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by Anna812 on 12/5/2016.
  */
 public class Deck {
+
+    ArrayList<Card> deck;
+
+    public Deck() {
+        deck = new ArrayList<>();
+        createNewDeck();
+    }
+
+    private void createNewDeck() {
+        ArrayList<String> possibleValues = new ArrayList<>(Arrays.asList("Ace", "2", "3", "4", "5", "6", "7", "8",
+                "9", "10", "Jack", "Queen", "King"));
+        ArrayList<String> possibleColours = new ArrayList<>(Arrays.asList("Clubs", "Diamonds", "Hearts", "Spades"));
+
+        for (String colour : possibleColours) {
+            for (String value : possibleValues) {
+                new Card(colour, value);
+            }
+        }
+    }
 }
